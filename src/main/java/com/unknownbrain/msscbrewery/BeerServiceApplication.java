7ebb9e205d5bc719022ccb2e0e2a5fb2ef4f8d18
Don <kartik15nov@gmail.com>
2020-07-16 20:43:13 +0530

    Changing the name of the service

--- src/main/java/com/unknownbrain/msscbrewery/BeerServiceApplication.java
+++ src/main/java/com/unknownbrain/msscbrewery/BeerServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsscBreweryApplication {
+public class BeerServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MsscBreweryApplication.class, args);
+        SpringApplication.run(BeerServiceApplication.class, args);
     }
 
 }
