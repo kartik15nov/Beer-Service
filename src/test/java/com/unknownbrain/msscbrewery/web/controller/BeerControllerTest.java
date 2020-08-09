2443bc8b2f8ae5b4c2246d5965679c656e4f9faa
Don <kartik15nov@gmail.com>
2020-08-09 23:59:54 +0530

    Adding RestDoc dependencies

--- src/test/java/com/unknownbrain/msscbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/com/unknownbrain/msscbrewery/web/controller/BeerControllerTest.java
@@ -6,7 +6,9 @@ import com.unknownbrain.msscbrewery.web.model.BeerDto;
 import com.unknownbrain.msscbrewery.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -21,6 +23,7 @@ import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
