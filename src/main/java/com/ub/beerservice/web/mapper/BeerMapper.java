6a8880ab670da3a1ef2042862a3e9ccb049daaf9
Kartik <kartik15nov@gmail.com>
2020-11-23 11:29:41 +0530

    Upgraded the Spring version from 2.3.1 to 2.4.0

--- src/main/java/com/ub/beerservice/web/mapper/BeerMapper.java
+++ src/main/java/com/ub/beerservice/web/mapper/BeerMapper.java
@@ -4,8 +4,6 @@ import com.ub.beerservice.domain.Beer;
 import com.ub.beerservice.web.model.BeerDto;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 
 @Mapper(uses = {DateMapper.class})
 @DecoratedWith(BeerMapperDecorator.class)
