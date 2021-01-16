a15d96c33fb45a289bd4f2bef7f7031bc1df56c7
Kartik <kartik15nov@gmail.com>
2021-01-16 11:41:30 +0530

    Added Hystrix Circuit breaker for Inventory Service failover. closes #33

---
+++ src/main/java/com/ub/beerService/services/inventory/InventoryFailoverFeignClient.java
@@ -0,0 +1,15 @@
+package com.ub.beerService.services.inventory;
+
+import com.ub.brewery.model.BeerInventoryDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(name = "inventory-failover")
+public interface InventoryFailoverFeignClient {
+
+    @GetMapping("/inventory-failover")
+    List<BeerInventoryDto> getOnHandInventory();
+}
---
+++ src/main/java/com/ub/beerService/services/inventory/InventoryServiceFeignClientFailover.java
@@ -0,0 +1,22 @@
+package com.ub.beerService.services.inventory;
+
+import com.ub.brewery.model.BeerInventoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Component
+public class InventoryServiceFeignClientFailover implements InventoryServiceFeignClient {
+
+    private final InventoryFailoverFeignClient inventoryFailoverFeignClient;
+
+    @Override
+    public ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(UUID beerId) {
+        return ResponseEntity.of(Optional.of(inventoryFailoverFeignClient.getOnHandInventory()));
+    }
+}
